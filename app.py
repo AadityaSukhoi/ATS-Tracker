a287103e29f31032b8960b854086d84d449dde21
Aaditya Ranjan Moitra <146163171+AadityaSukhoi@users.noreply.github.com>
2025-04-07 10:56:50 +0530

    Update app.py
    Changed the outdated model (gemini-pro) to (gemini-1.5-pro)

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 # GEMINI PRO RESPONSE
 def get_gemini_response(input):
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-pro')
     response = model.generate_content(input)
     return response.text
 
